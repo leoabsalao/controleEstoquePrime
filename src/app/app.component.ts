ef407773a8141a1d491c772135249ca09caed1a1
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-09-19 23:18:07 -0300

    Configuração do PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'controleEstoque';
+
+  constructor(private primeNgConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
+
 }
