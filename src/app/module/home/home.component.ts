52e852e3eb6d4f44393addfe0bf1743ec2196acf
FBS26 <leonardoabsalao@hotmail.com>
2024-09-24 15:37:01 -0300

    implementando as validações dos campos nos dois forms

--- src/app/module/home/home.component.ts
+++ src/app/module/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   loginCard = true;
-  
+
+  constructor(private formBuilder: FormBuilder){}
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onSubmitLoginForm(){
+    console.log('DADOS DO LOGIN FORM',this.loginForm.value);
+  }
+
+  onSubmitSignupForm(){
+    console.log('DADOS DO SIGNUP FORM',this.signupForm.value);
+  }
+
 }
