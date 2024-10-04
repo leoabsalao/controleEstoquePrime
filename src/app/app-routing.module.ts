8e38c1f861184433abbf7fbf61779b4d0e4d9d0c
Leonardo Absalão <100871641+leoabsalao@users.noreply.github.com>
2024-10-04 16:09:12 -0300

    iniciado a criação da camada de dashboard do projeto, com parte de rotas e redirecionamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './module/home/home.component';
+import { DashboardHomeComponent } from './module/dashboard/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo:'dashboard',
+    pathMatch:'full',
+  },
+  {
+    path: 'home',
     component: HomeComponent,
-  }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
+  },
 ];
 
 @NgModule({
